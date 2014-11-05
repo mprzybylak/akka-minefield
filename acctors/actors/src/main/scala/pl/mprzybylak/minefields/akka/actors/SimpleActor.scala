f122226cf8cf5693d9afb66164a06a22698ed7a7
MP <mp@mp.mp>
2014-11-05 14:27:45 +0100

    actor's companion object example

--- acctors/actors/src/main/scala/pl/mprzybylak/minefields/akka/actors/SimpleActor.scala
+++ acctors/actors/src/main/scala/pl/mprzybylak/minefields/akka/actors/SimpleActor.scala
@@ -1,6 +1,7 @@
 package pl.mprzybylak.minefields.akka.actors
 
 import akka.actor.Actor
+import akka.actor.Props
 
 /**
  * Actor extends akka.actor.Actor and have receive method.
@@ -16,4 +17,12 @@ class SimpleActor extends Actor {
 	  case _ 		=> println("unnown type of message")
 	}
   
+}
+
+/**
+ * It is a good practice to holds factory method returning proper configuration of Props instance
+ * in Actor's companion object
+ */
+object SimpleActor {
+  def props : Props = Props[SimpleActor]
 }
