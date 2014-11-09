5fb1fa98ee86eff21f0964413a86fd9935bc86d1
Michal Przybylak <michal.przybylak@gmail.com>
2014-11-09 12:34:53 +0100

    log test

--- random-arithmetic-mean/src/test/scala/pl/mprzybylak/minefields/akka/random/actors/RandomNumberActorTest.scala
+++ random-arithmetic-mean/src/test/scala/pl/mprzybylak/minefields/akka/random/actors/RandomNumberActorTest.scala
@@ -8,24 +8,36 @@ import org.scalatest.BeforeAndAfterAll
 import akka.testkit.TestActorRef
 import pl.mprzybylak.minefields.akka.random.messages.MeanMessage.RandomNumberMessage
 import scala.util.Random
+import com.typesafe.config.ConfigFactory
+import akka.testkit.EventFilter
 
-class RandomNumberActorTest extends TestKit(ActorSystem("mean-actor-system"))
+class RandomNumberActorTest extends TestKit(ActorSystem("mean-actor-system", ConfigFactory.parseString("""akka.loggers = ["akka.testkit.TestEventListener"]""")))
   with WordSpecLike
   with MustMatchers
   with BeforeAndAfterAll {
-  
-  
+
   "An actor" must {
-	 "genreate random number" in {
-		val ref = TestActorRef(RandomNumberActor.props(new Random { 
-		  override def nextInt(n:Int):Int = {
-		    return 10
-		  } 
-		}))
-		
-		ref ! RandomNumberMessage;
-	 }
+    "genreate random number" in {
+      val ref = TestActorRef(RandomNumberActor.props(new Random {
+        override def nextInt(n: Int): Int = {
+          return 10
+        }
+      }))
+
+      ref ! RandomNumberMessage;
+    }
+
+    "generate logs" in {
+      val ref = TestActorRef(RandomNumberActor.props(new Random {
+        override def nextInt(n: Int): Int = {
+          return 10
+        }
+      }))
+
+      EventFilter.info(pattern = "*", occurrences = 1) intercept {
+        ref ! RandomNumberMessage;
+      }
+    }
   }
-  
-  
+
 }
